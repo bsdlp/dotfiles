69994e3692f73cc2a9b4de07ae06ce11a80f7a0b
Jon Chen <bsd@voltaire.sh>
2014-06-25 17:31:45 -0400

    remove unused stuff

--- dotfiles/.vim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Patch dotvim configuration files on a new linux machine
-# (assuming requisite packages are already installed)
-#
-
-patch bundle/snipmate/snippets/c.snippets < c.snippets.diff
