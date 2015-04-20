8f59634a4c0ad879255211fc27a936c58cd25caf
Jon Chen <bsd@voltaire.sh>
2015-04-20 13:37:34 -0700

    add b2d alias

--- dotfiles/.zsh/docker.zsh
+++ dotfiles/.zsh/docker.zsh
@@ -2,3 +2,5 @@ function dc() {
     docker ps -a | awk '!/^CONT/ {print $1}' | xargs docker rm
     docker images | awk '!/^REPO/ {print $3}' | xargs docker rmi -f
 }
+
+alias b2d='$(boot2docker shellinit)'
