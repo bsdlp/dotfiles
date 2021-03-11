60f383e91a326bbfb050a2e6bdc6b8ee88fdf559
Jon Chen <bsd@voltaire.sh>
2021-03-11 00:32:17 -0800

    update docker functions

--- dotfiles/.zsh/docker.zsh
+++ dotfiles/.zsh/docker.zsh
@@ -1,6 +1,11 @@
+#function dc() {
+    #docker ps -a | awk '!/^CONT/ {print $1}' | xargs docker rm
+    #docker images | awk '!/^REPO/ {print $3}' | xargs docker rmi -f
+#}
+#
 function dc() {
-    docker ps -a | awk '!/^CONT/ {print $1}' | xargs docker rm
-    docker images | awk '!/^REPO/ {print $3}' | xargs docker rmi -f
+    docker container prune -f
+    docker image prune -a -f
 }
 
 alias dm='eval "$(docker-machine env default)"'
